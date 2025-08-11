056a96fdc03cddf838a42df2ffed61533361b2e1
Stefan Stikovich <stefan.sit@hotmail.com>
2025-08-11 11:00:53 +0200

    Setup Build

--- src/Snafets.TakeItEasy.Api/obj/Debug/net9.0/Snafets.TakeItEasy.Api.AssemblyInfo.cs
+++ src/Snafets.TakeItEasy.Api/obj/Debug/net9.0/Snafets.TakeItEasy.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snafets.TakeItEasy.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+873a96b2530cee174257b3aeac71c820a611b948")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snafets.TakeItEasy.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snafets.TakeItEasy.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
